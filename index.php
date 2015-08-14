8cd727316b5613fce3b8cf2542c50c142a856b6d
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 12:37:50 +0700

    insert foundation + reset page

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <html>
 
 <head>
-    <title>PTT : Blue Card (demo)</title>
+    <title>TrueSmart Activity</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="js/jquery.slimscroll.min.js"></script>
-    <script type="text/javascript" src="js/javascript.fullPage.js"></script>
-    <script type="text/javascript" src="js/jquery.cover.js"></script>
-    <script type="text/javascript" src="js/slippry.min.js"></script>
-    <link rel="stylesheet" href="css/javascript.fullPage.css">
-    <link rel="stylesheet" href="css/jquery.cover.css">
-    <link rel="stylesheet" href="css/slippry.css" />
+    <script src="js/foundation.min.js"></script>
+    <script src="js/vendor/modernizr.js"></script>
+    <script src="js/vendor/fastclick.js"></script>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <?php include("views/header.php"); ?>
-        <?php include("views/content.php"); ?>
-            <?php include("views/footer.php"); ?>
-                <script type="text/javascript" src="js/main.js"></script>
+    <div id="wrapper">
+        <?php //include("views/header.php"); ?>
+            <?php include("views/content.php"); ?>
+                <?php //include("views/footer.php"); ?>
+                    <script type="text/javascript" src="js/main.js"></script>
+                    <!-- end wrap -->
+    </div>
 </body>
 
 </html>
