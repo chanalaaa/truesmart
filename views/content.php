1264ddf57927fbdf691674f711665a0f22206d19
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 13:03:44 +0700

    insert grid

--- views/content.php
+++ views/content.php
@@ -1,29 +1,67 @@
-<div class="menu">
-    <ul>
-        <a href="#">
-            <li>I. Home</li>
-        </a>
-        <a href="#">
-            <li>II. About</li>
-        </a>
-        <a href="#">
-            <li>III. Services</li>
-        </a>
-        <a href="#">
-            <li>IV. Blog</li>
-        </a>
-        <a href="#">
-            <li>VI. Contact</li>
-        </a>
-    </ul>
+<div class="row">
+    <div class="small-12 columns">
+        <div class="row">
+            <div class="small-12 columns">
+                <div class="menu">
+                    <ul>
+                        <a href="#">
+                            <li>I. Home</li>
+                        </a>
+                        <a href="#">
+                            <li>II. About</li>
+                        </a>
+                        <a href="#">
+                            <li>III. Services</li>
+                        </a>
+                        <a href="#">
+                            <li>IV. Blog</li>
+                        </a>
+                        <a href="#">
+                            <li>VI. Contact</li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="menu-toggle"> <a href="#"><i class="fa fa-angle-double-down"></i> v </a> </div>
+            </div>
+        </div>
+    </div>
 </div>
-<div class="menu-toggle"> <a href="#"><i class="fa fa-angle-double-down"></i> v </a> </div>
+
+<div class="row">
+    <div class="small-12 columns">
+        <div class="row">
+            <div class="small-12 columns">
+                <div class="menu">
+                    <ul>
+                        <a href="#">
+                            <li>I. Home</li>
+                        </a>
+                        <a href="#">
+                            <li>II. About</li>
+                        </a>
+                        <a href="#">
+                            <li>III. Services</li>
+                        </a>
+                        <a href="#">
+                            <li>IV. Blog</li>
+                        </a>
+                        <a href="#">
+                            <li>VI. Contact</li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="menu-toggle"> <a href="#"><i class="fa fa-angle-double-down"></i> v </a> </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 <div class="about">blah blah blah</div>
 <!-- SCRIPT ******************************** -->
 <script type="text/javascript">
 $(document).ready(function() {
     $(".menu-toggle a").click(function() {
         $(".menu").slideToggle(700);
-    }); 
+    });
 });
 </script>
