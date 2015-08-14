c4da2468ae169ae29c2ad08f56981fc4dbc23391
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 16:50:27 +0700

    content-info + content-toggle phase1

--- views/content.php
+++ views/content.php
@@ -2,12 +2,13 @@
     <div class="row">
         <div class="small-12 columns">
             <div class="content-pre_block">
+                <div class="content-pre_block_opacity"></div>
                 <div class="content-pre_info">
                     <p>ซิตีแผดเผาลามะสุนทรีย์ โมจิแจม ซ้อเนิร์สเซอรี่สติ๊กเกอร์แจ๊สวอลนัต มอลล์บ๊อบไลน์เซนเซอร์ เห่ยซูม สปอตสแตนเลสเสือโคร่ง เบบี้บาร์บีคิวแช่แข็ง ไทเฮาเจ็ตปิกอัพ ฟิวเจอร์กลาสนอมินีเวิลด์ น้องใหม่ตื้บบรรพชนราชบัณฑิตยสถานคอนแทค แบล็กแฟรีรายชื่อ Home</p>
-                    <div class="menu-toggle"> <a href="#"> v </a> </div>
+                    <div class="content-toggle_btn"><a href="#"> v </a> </div>
                 </div>
             </div>
-            <div class="menu">
+            <div class="content-toggle">
                 <ul>
                     <li><a href="#">I. Home</a></li>
                     <li><a href="#">II. About</a></li>
@@ -21,45 +22,51 @@
 </div>
 <div class="content-block">
     <div class="row">
+        <div class="small-12 columns">
+            <div class="content-pre_block">
+                <div class="content-pre_block_opacity"></div>
+                <div class="content-pre_info">
+                    <p>ไชน่าโกเต็กซ์ตุ๊ดโจ๋เอาท์ นรีแพทย์มยุราภิรมย์ซีน โรแมนติก ไฮเทคโบกี้มหาอุปราชาวอล์คเซรามิก เรซินคอปเตอร์แอโรบิคมอลล์ เช็งเม้ง ฟอยล์โรลออนกับดักบลอนด์ หลวงปู่แคร์ด็อกเตอร์หลวงพี่</p>
+                    <div class="content-toggle_btn"><a href="#"> v </a> </div>
+                </div>
+            </div>
+            <div class="content-toggle">
+                <ul>
+                    <li><a href="#">I. Home</a></li>
+                    <li><a href="#">II. About</a></li>
+                    <li><a href="#">III. Services</a></li>
+                    <li><a href="#">IV. Blog</a></li>
+                    <li><a href="#">VI. Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--
+<div class="content-block">
+    <div class="row">
         <div class="content-block">
             <div class="small-12 columns">
-                <div class="menu-toggle"> <a href="#"> v </a> </div>
-                <div class="menu">
-                    <ul>
+                <div class="content-toggle_btn"> <a href="#"> v </a> </div>
+                <div class="content-toggle">
+                    <ul class="">
                         <li><a href="#">I. Home</a></li>
                         <li><a href="#">II. About</a></li>
                         <li><a href="#">III. Services</a></li>
                         <li><a href="#">IV. Blog</a></li>
-                        <li><a href="#">VI. Contact</a></li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="content-block">
-<div class="row">
-    <div class="content-block">
-        <div class="small-12 columns">
-            <div class="menu-toggle"> <a href="#"> v </a> </div>
-            <div class="menu">
-                <ul class="">
-                    <li><a href="#">I. Home</a></li>
-                    <li><a href="#">II. About</a></li>
-                    <li><a href="#">III. Services</a></li>
-                    <li><a href="#">IV. Blog</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
+-->
 <div class="about">blah blah blah</div>
 <!-- SCRIPT ******************************** -->
 <script type="text/javascript">
 $(document).ready(function() {
-    $(".menu-toggle a").click(function() {
-        $(this).parent().parent().parent().parent().children(".menu").slideToggle(700);
+    $(".content-toggle_btn a").click(function() {
+        $(this).parent().parent().parent().parent().children(".content-toggle").slideToggle(700);
     });
 });
 </script>
