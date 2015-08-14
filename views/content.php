bef98b2aee3fa62549a861209c2575d093974ded
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 12:39:24 +0700

    insert test content

--- views/content.php
+++ views/content.php
@@ -1,24 +1,29 @@
-
- 
-
+<div class="menu">
+    <ul>
+        <a href="#">
+            <li>I. Home</li>
+        </a>
+        <a href="#">
+            <li>II. About</li>
+        </a>
+        <a href="#">
+            <li>III. Services</li>
+        </a>
+        <a href="#">
+            <li>IV. Blog</li>
+        </a>
+        <a href="#">
+            <li>VI. Contact</li>
+        </a>
+    </ul>
+</div>
+<div class="menu-toggle"> <a href="#"><i class="fa fa-angle-double-down"></i> v </a> </div>
+<div class="about">blah blah blah</div>
+<!-- SCRIPT ******************************** -->
 <script type="text/javascript">
-fullpage.initialize('#fullpage', {
-    anchors: ['firstPage'],
-    menu: '#menu',
-    sectionsColor: ['#4A6FB1'],
-    autoScrolling: false,
-    css3: true,
-    fitToSection: false
-});
-
-
-
-$(function() {
-
-    $("#promotion-slide_ul").slippry({
-        transition: 'horizontal'
+$(document).ready(function() {
+    $(".menu-toggle a").click(function() {
+        $(".menu").slideToggle(700);
     });
-
-   
 });
 </script>
