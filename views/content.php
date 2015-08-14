01916f72f06d6f10112ac457a62b51543d58407e
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 19:47:05 +0700

    Finish header img + add header logo

--- views/content.php
+++ views/content.php
@@ -1,12 +1,11 @@
 <div class="header">
     <div class="header-block">
-    <div class="header_img"></div>
+        <div class="header_img"></div>
+        <div class="header_logo">
+            <img src="img/header/header_logo.png">
+        </div>
     </div>
-
 </div>
-
-
-
 <div class="content-block">
     <div class="row">
         <div class="small-12 columns">
@@ -14,7 +13,9 @@
                 <div class="content-pre_info">
                     <p>ซิตีแผดเผาลามะสุนทรีย์ โมจิแจม ซ้อเนิร์สเซอรี่สติ๊กเกอร์แจ๊สวอลนัต มอลล์บ๊อบไลน์เซนเซอร์ เห่ยซูม สปอตสแตนเลสเสือโคร่ง เบบี้บาร์บีคิวแช่แข็ง ไทเฮาเจ็ตปิกอัพ ฟิวเจอร์กลาสนอมินีเวิลด์ น้องใหม่ตื้บบรรพชนราชบัณฑิตยสถานคอนแทค แบล็กแฟรีรายชื่อ Home</p>
                 </div>
-                <div class="content-toggle_btn"><a href="#"><img src="img/content/expand_btn.png" alt="more..."></a> </div>
+                <div class="content-toggle_btn">
+                    <a href="javascript:;"><img src="img/content/expand_btn.png" alt="more..."></a>
+                </div>
             </div>
             <div class="content-toggle">
                 <ul>
@@ -35,7 +36,9 @@
                 <div class="content-pre_info">
                     <p>ไชน่าโกเต็กซ์ตุ๊ดโจ๋เอาท์ นรีแพทย์มยุราภิรมย์ซีน โรแมนติก ไฮเทคโบกี้มหาอุปราชาวอล์คเซรามิก เรซินคอปเตอร์แอโรบิคมอลล์ เช็งเม้ง ฟอยล์โรลออนกับดักบลอนด์ หลวงปู่แคร์ด็อกเตอร์หลวงพี่</p>
                 </div>
-                <div class="content-toggle_btn"><a href="#"><img src="img/content/expand_btn.png" alt="more..."></a> </div>
+                <div class="content-toggle_btn">
+                    <a href="javascript:;"><img src="img/content/expand_btn.png" alt="more..."></a>
+                </div>
             </div>
             <div class="content-toggle">
                 <ul>
