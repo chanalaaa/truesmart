aebf6a195c16825d512ae5b53f3cc89a7f9c2a9c
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 16:16:59 +0700

    insert content-block

--- views/content.php
+++ views/content.php
@@ -1,7 +1,12 @@
-<div class="row">
-    <div class="content-block">
-        <div class="small-12   columns">
-         <div class="menu-toggle"> <a href="#"><i class="fa fa-angle-double-down"></i> v </a> </div>
+<div class="content-block">
+    <div class="row">
+        <div class="small-12 columns">
+            <div class="content-pre_block">
+                <div class="content-pre_info">
+                    <p>ซิตีแผดเผาลามะสุนทรีย์ โมจิแจม ซ้อเนิร์สเซอรี่สติ๊กเกอร์แจ๊สวอลนัต มอลล์บ๊อบไลน์เซนเซอร์ เห่ยซูม สปอตสแตนเลสเสือโคร่ง เบบี้บาร์บีคิวแช่แข็ง ไทเฮาเจ็ตปิกอัพ ฟิวเจอร์กลาสนอมินีเวิลด์ น้องใหม่ตื้บบรรพชนราชบัณฑิตยสถานคอนแทค แบล็กแฟรีรายชื่อ Home</p>
+                    <div class="menu-toggle"> <a href="#"> v </a> </div>
+                </div>
+            </div>
             <div class="menu">
                 <ul>
                     <li><a href="#">I. Home</a></li>
@@ -11,14 +16,32 @@
                     <li><a href="#">VI. Contact</a></li>
                 </ul>
             </div>
-           
         </div>
     </div>
-    </div>
+</div>
+<div class="content-block">
     <div class="row">
+        <div class="content-block">
+            <div class="small-12 columns">
+                <div class="menu-toggle"> <a href="#"> v </a> </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="#">I. Home</a></li>
+                        <li><a href="#">II. About</a></li>
+                        <li><a href="#">III. Services</a></li>
+                        <li><a href="#">IV. Blog</a></li>
+                        <li><a href="#">VI. Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content-block">
+<div class="row">
     <div class="content-block">
         <div class="small-12 columns">
-        <div class="menu-toggle"> <a href="#"><i class="fa fa-angle-double-down"></i> v </a> </div>
+            <div class="menu-toggle"> <a href="#"> v </a> </div>
             <div class="menu">
                 <ul class="">
                     <li><a href="#">I. Home</a></li>
@@ -30,12 +53,13 @@
         </div>
     </div>
 </div>
+</div>
 <div class="about">blah blah blah</div>
 <!-- SCRIPT ******************************** -->
 <script type="text/javascript">
 $(document).ready(function() {
     $(".menu-toggle a").click(function() {
-        $(this).parent().parent().children(".menu").slideToggle(700);
+        $(this).parent().parent().parent().parent().children(".menu").slideToggle(700);
     });
 });
 </script>
