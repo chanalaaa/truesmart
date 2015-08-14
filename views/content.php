4c832ad5d059b5b2a3eaa7849fc6f93298ea7be3
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 18:40:04 +0700

    header phase1

--- views/content.php
+++ views/content.php
@@ -1,3 +1,12 @@
+<div class="header">
+    <div class="header-block">
+    <div class="header_img"></div>
+    </div>
+
+</div>
+
+
+
 <div class="content-block">
     <div class="row">
         <div class="small-12 columns">
@@ -5,7 +14,7 @@
                 <div class="content-pre_info">
                     <p>ซิตีแผดเผาลามะสุนทรีย์ โมจิแจม ซ้อเนิร์สเซอรี่สติ๊กเกอร์แจ๊สวอลนัต มอลล์บ๊อบไลน์เซนเซอร์ เห่ยซูม สปอตสแตนเลสเสือโคร่ง เบบี้บาร์บีคิวแช่แข็ง ไทเฮาเจ็ตปิกอัพ ฟิวเจอร์กลาสนอมินีเวิลด์ น้องใหม่ตื้บบรรพชนราชบัณฑิตยสถานคอนแทค แบล็กแฟรีรายชื่อ Home</p>
                 </div>
-                <div class="content-toggle_btn"><a href="#"> v </a> </div>
+                <div class="content-toggle_btn"><a href="#"><img src="img/content/expand_btn.png" alt="more..."></a> </div>
             </div>
             <div class="content-toggle">
                 <ul>
@@ -26,7 +35,7 @@
                 <div class="content-pre_info">
                     <p>ไชน่าโกเต็กซ์ตุ๊ดโจ๋เอาท์ นรีแพทย์มยุราภิรมย์ซีน โรแมนติก ไฮเทคโบกี้มหาอุปราชาวอล์คเซรามิก เรซินคอปเตอร์แอโรบิคมอลล์ เช็งเม้ง ฟอยล์โรลออนกับดักบลอนด์ หลวงปู่แคร์ด็อกเตอร์หลวงพี่</p>
                 </div>
-                <div class="content-toggle_btn"><a href="#"> v </a> </div>
+                <div class="content-toggle_btn"><a href="#"><img src="img/content/expand_btn.png" alt="more..."></a> </div>
             </div>
             <div class="content-toggle">
                 <ul>
