00fa18c2b19e8fb481f129a9da4b6f38f9eec52b
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-14 18:01:02 +0700

    add list style in <li>

--- views/content.php
+++ views/content.php
@@ -2,11 +2,10 @@
     <div class="row">
         <div class="small-12 columns">
             <div class="content-pre_block">
-                <div class="content-pre_block_opacity"></div>
                 <div class="content-pre_info">
                     <p>ซิตีแผดเผาลามะสุนทรีย์ โมจิแจม ซ้อเนิร์สเซอรี่สติ๊กเกอร์แจ๊สวอลนัต มอลล์บ๊อบไลน์เซนเซอร์ เห่ยซูม สปอตสแตนเลสเสือโคร่ง เบบี้บาร์บีคิวแช่แข็ง ไทเฮาเจ็ตปิกอัพ ฟิวเจอร์กลาสนอมินีเวิลด์ น้องใหม่ตื้บบรรพชนราชบัณฑิตยสถานคอนแทค แบล็กแฟรีรายชื่อ Home</p>
-                    <div class="content-toggle_btn"><a href="#"> v </a> </div>
                 </div>
+                <div class="content-toggle_btn"><a href="#"> v </a> </div>
             </div>
             <div class="content-toggle">
                 <ul>
@@ -24,11 +23,10 @@
     <div class="row">
         <div class="small-12 columns">
             <div class="content-pre_block">
-                <div class="content-pre_block_opacity"></div>
                 <div class="content-pre_info">
                     <p>ไชน่าโกเต็กซ์ตุ๊ดโจ๋เอาท์ นรีแพทย์มยุราภิรมย์ซีน โรแมนติก ไฮเทคโบกี้มหาอุปราชาวอล์คเซรามิก เรซินคอปเตอร์แอโรบิคมอลล์ เช็งเม้ง ฟอยล์โรลออนกับดักบลอนด์ หลวงปู่แคร์ด็อกเตอร์หลวงพี่</p>
-                    <div class="content-toggle_btn"><a href="#"> v </a> </div>
                 </div>
+                <div class="content-toggle_btn"><a href="#"> v </a> </div>
             </div>
             <div class="content-toggle">
                 <ul>
@@ -66,7 +64,7 @@
 <script type="text/javascript">
 $(document).ready(function() {
     $(".content-toggle_btn a").click(function() {
-        $(this).parent().parent().parent().parent().children(".content-toggle").slideToggle(700);
+        $(this).parent().parent().parent().children(".content-toggle").slideToggle(500);
     });
 });
 </script>
