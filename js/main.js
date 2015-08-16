ff823394be7462355d942366e258a44c936b0458
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-17 00:50:52 +0700

    add+comment  func. in main.js to setting animate toggle on window width

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
+    if ($(window).width() <= 1000) {
+        $time = 0;
+    } else if ($(window).width() > 1000) {
+        $time = 500;
+    }
+
+    $(window).resize(function() {
+        if ($(window).width() <= 1000) {
+            $time = 0;
+        } else {
+            $time = 500;
+        }
+    });
+
     $(".content-toggle_btn a").click(function() {
-        $(this).parent().parent().parent().children(".content-toggle").slideToggle(500);
+        $(this).parent().parent().parent().children(".content-toggle").slideToggle($time);
+        /*
+               for mobile device
+               -device need to reduce animate for prevention of out of ram problem.
+           */
     });
 });
