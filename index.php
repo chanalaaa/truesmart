4f8255b90cfbccb509d6553faa295c32a2adb9d7
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-16 22:23:15 +0700

    open gzip func. + cache js,css

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
-<html>
+<?php
+ob_start('ob_gzhandler');
+?>
+    <html>
 
-<head>
-    <title>TrueSmart Activity</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="js/vendor/fastclick.js"></script>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+    <head>
+        <title>TrueSmart Activity</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
+        <script src="js/foundation.min.js?v=1001"></script>
+        <script src="js/vendor/modernizr.js?v=1001"></script>
+        <script src="js/vendor/fastclick.js?v=1001"></script>
+        <link rel="stylesheet" href="css/normalize.css?v=1001">
+        <link rel="stylesheet" href="css/foundation.css?v=1001">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
 
-<body>
-    <div id="wrapper">
-        <?php include("views/header.php"); ?>
-            <?php include("views/content.php"); ?>
-                <?php include("views/footer.php"); ?>
-                    <script type="text/javascript" src="js/main.js"></script>
-                    
-    </div>
-</body>
+    <body>
+        <div id="wrapper">
+            <?php include("views/header.php"); ?>
+                <?php include("views/content.php"); ?>
+                    <?php include("views/footer.php"); ?>
+                        <script type="text/javascript" src="js/main.js"></script>
+        </div>
+    </body>
 
-</html>
+    </html>
