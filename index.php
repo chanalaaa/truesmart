a1232ce3aeb1a4ebea9d0d03183c2a4899404811
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-20 18:17:04 +0700

    optimize phase1

--- index.php
+++ index.php
@@ -8,15 +8,16 @@ ob_start('ob_gzhandler');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js?v=1001"></script>
         <script src="js/foundation.min.js?v=1001"></script>
-        <script src="js/vendor/modernizr.js?v=1001"></script>
-        <script src="js/vendor/fastclick.js?v=1001"></script>
-        <link rel="stylesheet" href="css/normalize.css?v=1001">
+        <link rel="stylesheet" href="css/normalize.min.css?v=1001">
+        <link rel="stylesheet" href="css/jquery-ui.min.css">
         <link rel="stylesheet" href="css/foundation.css?v=1001">
         <link rel="stylesheet" href="css/style.css">
+        <!--
+        <script src="js/vendor/modernizr.js?v=1001"></script>
+        <script src="js/vendor/fastclick.js?v=1001"></script>
+        -->
     </head>
 
     <body>
