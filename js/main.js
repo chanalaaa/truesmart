a33890170294f1432cd8c1c84bbf3094ac056971
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-20 10:58:04 +0700

    add js rotate arrow btn (.content-toggle_btn)

--- js/main.js
+++ js/main.js
@@ -14,11 +14,32 @@ $(document).ready(function() {
         }
     });
 
+    $bool = 1;
+    //$roll = 180;
     $(".content-toggle_btn a").click(function() {
         $(this).parent().parent().parent().children(".content-toggle").slideToggle($time);
         /*
-               for mobile device
-               -device need to reduce animate for prevention of out of ram problem.
-           */
+              for mobile device
+              -device need to reduce animate for prevention of out of ram problem.
+          */
+
+        if ($bool == 1) {
+            $(this).children("img").css({
+               // "-ms-transform": "rotate("+$roll+"deg)",
+               "-ms-transform": "rotate(180deg)",
+                "-webkit-transform:": "rotate(180deg)",
+                "transform": "rotate(180deg)"
+            });
+            $bool = 0;
+        } else {
+            $(this).children("img").css({
+                "-ms-transform": "rotate(0deg)",
+                "-webkit-transform:": "rotate(0deg)",
+                "transform": "rotate(0deg)"
+            });
+
+            $bool = 1;
+        }
+
     });
 });
